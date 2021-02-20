21152362b3696e7d7b81a7a3f137af6b9614aa27
ROBLOX Team Foundation <github@roblox.com>
2021-02-20 06:28:34 +0000

    Remove these

--- src/Helpers/Internal/executeRequestInternal.ts
+++
@@ -1,6 +0,0 @@
-import { HttpRequest } from '../../Classes/HttpRequest';
-import { Response } from 'request';
-
-export const executeRequest = async (request: HttpRequest): Promise<Response> => {
-	return await request.execute(this);
-};
--- src/util/customTypes.ts
+++
@@ -1,3 +0,0 @@
-import { DataStore } from '../Classes/DataStore';
-
-export type DataStores = Map<string, DataStore>;
