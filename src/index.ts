6f9893805fc20f0d11f8e0f0305a1e464423492d
ROBLOX Team Foundation <github@roblox.com>
2021-03-08 21:21:47 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export { init as InitializeAsync } from './Helpers/InitAuthenticatedUser';
-import { _MessagingService } from './Services/MessagingService';
-export const MessagingService = new _MessagingService();
+export { _MessagingService as MessagingService } from './Services/MessagingService';
